c1f961ffe2e16ee2b186fc8e9c7b1f5a90a48453
T0modachi <jvergarava@gmail.com>
2022-06-18 23:51:59 -0400

    oaq factory config for sjms2 and add dependencies

--- src/main/java/example/MainApp.java
+++ src/main/java/example/MainApp.java
@@ -1,19 +1,33 @@
 package example;
 
-import org.apache.camel.main.Main;
+import javax.jms.JMSException;
+import javax.jms.XAQueueConnectionFactory;
+import oracle.jms.AQjmsFactory;
+import org.apache.camel.component.sjms2.Sjms2Component;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
 
 /**
  * A Camel Application
  */
 public class MainApp {
 
-    /**
-     * A main() so we can easily run these routing rules in our IDE
-     */
     public static void main(String... args) throws Exception {
-        Main main = new Main();
-        main.addRouteBuilder(new MyRouteBuilder());
-        main.run(args);
+        CamelContext context = new DefaultCamelContext();
+        Sjms2Component sjmsComponent = new Sjms2Component();
+        try {
+            sjmsComponent.setConnectionFactory(getXAQueueConnectionFactory());
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+        context.addComponent("sjms2", sjmsComponent);
+        context.addRoutes(new MyRouteBuilder());
+        context.start();
+   }
+    
+    static XAQueueConnectionFactory getXAQueueConnectionFactory() throws JMSException {
+        return  AQjmsFactory.getXAQueueConnectionFactory("jdbc:oracle:thin:USER/PASS@DB_HOST:1521:DB",null);
     }
 
 }

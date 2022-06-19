7a6ecafd1413186199f19d70d797704cac44d1ff
T0modachi <jvergarava@gmail.com>
2022-06-18 23:34:24 -0400

    configure a sjms2 route to log

--- src/main/java/example/MyRouteBuilder.java
+++ src/main/java/example/MyRouteBuilder.java
@@ -12,17 +12,8 @@ public class MyRouteBuilder extends RouteBuilder {
      */
     public void configure() {
 
-        // here is a sample which processes the input files
-        // (leaving them in place - see the 'noop' flag)
-        // then performs content based routing on the message using XPath
-        from("file:src/data?noop=true")
-            .choice()
-                .when(xpath("/person/city = 'London'"))
-                    .log("UK message")
-                    .to("file:target/messages/uk")
-                .otherwise()
-                    .log("Other message")
-                    .to("file:target/messages/others");
+        from("sjms2:queue:YOUR_OAQ?transacted=true&transactionBatchCount=10")
+                .to("log:output?level=INFO");
     }
 
 }
